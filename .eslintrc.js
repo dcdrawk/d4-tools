9a3fa7d1eefdb031f33d83e9609d9319c6dbaff8
devin <devin@thewebdev.ca>
2023-03-25 10:09:42 -0700

    init commit

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 'latest'
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'plugin:nuxt/recommended',
+    '@nuxtjs/eslint-config-typescript'
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/max-attributes-per-line': ['error', {
+      singleline: {
+        max: 1
+      },
+      multiline: {
+        max: 1
+      }
+    }]
+  }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+import colors from 'tailwindcss/colors'
+
+module.exports = {
+  content: [
+    './**/*.html',
+    './**/*.vue',
+    './**/*.jsx'
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+    fontFamily: {
+      display: ['Rubik', 'sans-serif'],
+      body: ['Open Sans', 'sans-serif']
+    },
+    container: {
+      center: true,
+      screens: {
+        sm: '100%',
+        md: '100%',
+        lg: '1024px',
+        xl: '1280px'
+      }
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      blue: colors.blue,
+      white: colors.white,
+      gray: colors.neutral,
+      indigo: colors.indigo,
+      emerald: colors.emerald,
+      red: colors.rose,
+      yellow: colors.amber
+    }
+  },
+  variants: {
+    extend: {
+      borderWidth: ['last']
+    }
+  },
+  plugins: [],
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true
+  }
+}
